e5eaed9cf673c494d2bf5ce54c052a5a17d0b9aa
Corentin Deseine <deseinecorentin@gmail.com>
2022-04-20 11:08:44 +0200

    yelp mvp with form checked and links done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
-                                    message: '%<category> is not a valid size' }
+  CATEGORY_LIST = %w[chinese italian japanese french belgian].freeze
+
+  validates :category, inclusion: { in: CATEGORY_LIST }
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
